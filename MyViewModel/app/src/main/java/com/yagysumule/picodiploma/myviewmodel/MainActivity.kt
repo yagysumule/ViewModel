973b509de39a2f59c02a22861aac9157b3119bf9
yagysumule <codewithdiyah@gmail.com>
2021-04-30 15:14:43 +0700

    Initial commit

---
+++ MyViewModel/app/src/main/java/com/yagysumule/picodiploma/myviewmodel/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.yagysumule.picodiploma.myviewmodel
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.yagysumule.picodiploma.myviewmodel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: WeatherAdapter
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = WeatherAdapter()
+        adapter.notifyDataSetChanged()
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+        mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+
+        binding.btnCity.setOnClickListener {
+            val city = binding.editCity.text.toString()
+
+            if (city.isEmpty()) return@setOnClickListener
+
+            showLoading(true)
+            mainViewModel.setWeather(city)
+        }
+
+        mainViewModel.getWeathers().observe(this, { weatherItems ->
+            if (weatherItems != null) {
+                adapter.setData(weatherItems)
+                showLoading(false)
+            }
+        })
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Tujuan
+Pada Codelab kali ini Anda akan mempelajari bagaimana mengimplementasikan ViewModel dalam membuat aplikasi Android. Hasil dari codelab kali ini akan menjadi seperti ini:
+
+![MyViewModel](https://user-images.githubusercontent.com/68750843/116657371-7bbd1d00-a9b8-11eb-8a4b-60c2822cbe42.gif)
+
+# Logika Dasar
+Melakukan input → mengirim data ke ViewModel → request ke API → mendapatkan list item → mengirim data ke Activity → melakukan perubahan rotasi → data masih terjaga.
